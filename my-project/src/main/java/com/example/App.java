2996a2a376b71827be637ed70003ec23f69d1704
rsvi3095 <ravi3095@gmail.com>
2022-01-08 16:39:36 -0800

    test

--- my-project/src/main/java/com/example/App.java
+++ my-project/src/main/java/com/example/App.java
@@ -11,7 +11,7 @@ public class App {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(App.class, args);
+         SpringApplication.run(App.class, args);
     }
 
     @PostConstruct
